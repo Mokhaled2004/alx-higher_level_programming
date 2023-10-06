776b87e314f9d6588d01b0e8136abadaf7eee72c
Mokhaled2004 <mokhaled2004@gmail.com>
2023-10-06 04:13:44 -0700

    Nanananami

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = _import_('magic_calculation_102').add
-    sub = _import_('magic_calculation_102').sub
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
 
     if a < b:
         c = add(a, b)
