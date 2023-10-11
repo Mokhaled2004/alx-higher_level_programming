a345c2edb2e5a9900840cb74febd391ea25d8568
Mokhaled2004 <mokhaled2004@gmail.com>
2023-10-11 10:21:39 -0700

    Nanananami

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,10 @@
 
 
 def search_replace(my_list, search, replace):
-    
+    """
+    A function that replaces all occurrences
+    of an element by another element
+    """
     new_list = []
     for element in my_list:
         if element == search:
